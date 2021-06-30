51b864608c9e1a302ad49128ccab97980ec9912f
Alien <kendgibsonjr@gmail.com>
2021-06-30 06:06:05 -0500

    imported model files

---
+++ client/build/precache-manifest.f84c9ea75b4bdeaecfd8c8c064fb8a73.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9dcdc287ccd4c155f1fbdb47d6e5ea11",
+    "url": "/index.html"
+  },
+  {
+    "revision": "299a9b0dc466891b62a1",
+    "url": "/static/css/main.bdded9b7.chunk.css"
+  },
+  {
+    "revision": "d41323bb2e7f8da04d9c",
+    "url": "/static/js/2.98126eaa.chunk.js"
+  },
+  {
+    "revision": "9be8f09f0bfe1d20164d4c42e389b466",
+    "url": "/static/js/2.98126eaa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "299a9b0dc466891b62a1",
+    "url": "/static/js/main.0b746469.chunk.js"
+  },
+  {
+    "revision": "d08766e1dd2bf4f6c209",
+    "url": "/static/js/runtime-main.992eac29.js"
+  }
+]);
